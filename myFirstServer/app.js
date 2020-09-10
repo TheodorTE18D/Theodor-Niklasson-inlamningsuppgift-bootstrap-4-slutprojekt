9c46b1f427c79487f36786e053c9859c67d38ac6
TheodorTE18D <54800526+TheodorTE18D@users.noreply.github.com>
2020-09-10 12:49:14 +0200

    Labb Post

--- myFirstServer/app.js
+++ myFirstServer/app.js
@@ -4,12 +4,21 @@ const port = 3000
 
 const clientDir = __dirname + "\\client\\"
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.get('/', (req, res) => res.sendFile(clientDir + "index.html"))
-app.get('/style', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(clientDir + "styleIndex.css")
 })
 app.get('/zombies', (req, res) => {
     res.sendFile(clientDir + "zombie.png")
   })
+
+app.post('/', (req, res)  =>  {
+    console.log(req.body.name)
+    console.log(req.body.email)
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
